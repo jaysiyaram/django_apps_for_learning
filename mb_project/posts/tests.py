0e05e9cdad3c98ebd9e90ba547d52dbfdb815b14
Ramanuj Sharma <ramanuj.sharma512@gmail.com>
2018-04-16 22:10:14 +0530

    Wrote testcases for posts app

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text="Test for testing")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_value = '{0}'.format(post.text)
+        self.assertEqual(expected_value, 'Test for testing')
+
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text="Another text for testing")
+
+    def test_url_by_location(self):
+        resp = self.client.get('/')
+        # import pdb; pdb.set_trace()
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
